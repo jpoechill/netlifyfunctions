f1338d1c381f9ce9ef8d869dbd55ca302f7f54f2
Po Rith <poo@Poamrongs-MBP.attlocal.net>
2019-07-04 22:44:16 -0700

    update functions

--- functions/custom-test.js
+++ functions/custom-test.js
@@ -1,6 +1,13 @@
+import faunadb from 'faunadb' /* Import faunaDB sdk */
+
+process.env.FAUNADB_SERVER_SECRET = 'fnADShQvS9ACAHyTZ1e6xHU9TCwXuwyN6N2o2HaU'
+process.env.FAUNADB_SECRET = 'fnADShQvS9ACAHyTZ1e6xHU9TCwXuwyN6N2o2HaU'
+
+
 exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: 200,
-        body: 'No worries, all is working fine!'
+        body: 'Everything is okay!!'
     })
 }
+
