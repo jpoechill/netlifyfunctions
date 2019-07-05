40e20f6769dc5faf676f11ada1118b50548519db
Po Rith <poo@Poamrongs-MBP.attlocal.net>
2019-07-04 23:29:55 -0700

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="container">
-    123
+    <button @click="handleClick()">
+      Create ToDo
+    </button>
   </section>
 </template>
 
@@ -10,39 +12,32 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  methods: {
+    handleClick () {
+      const myTodo = {
+        title: 'My todo title',
+        completed: false,
+      }
+
+      this.createTodo(myTodo).then((response) => {
+        console.log('API response', response)
+        // set app state
+      }).catch((error) => {
+        console.log('API error', error)
+      })
+    },
+    createTodo(data) {
+      return this.$axios.get('/.netlify/functions/test')
+      .then(response => {
+        console.log('Successfully FETCHED')
+        return response
+      })
+    }
   }
 }
+
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
