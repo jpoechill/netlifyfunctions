68e2433b1a2400dec86c7e991e6aca69c931ee65
Po Rith <poo@Poamrongs-MBP.attlocal.net>
2019-07-04 23:43:04 -0700

    commit

--- functions/custom-test.js
+++ functions/custom-test.js
@@ -17,10 +17,28 @@ exports.handler = (event, context, callback) => {
       data: data
     }
 
-    callback(null, {
+    return client.query(q.Create(q.Ref('classes/todos'), todoItem))
+    .then((response) => {
+      console.log('success', response)
+      /* Success! return the response with statusCode 200 */
+      
+      return callback(null, {
         statusCode: 200,
-        body: 'Everything is okay 10!!'
+        body: JSON.stringify(response)
+      })
+    }).catch((error) => {
+      console.log('error', error)
+      /* Error! return the error with statusCode 400 */
+
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      })
     })
+    // callback(null, {
+    //     statusCode: 200,
+    //     body: 'Everything is okay 10!!'
+    // })
 }
 /* configure faunaDB Client with our secret */
 
