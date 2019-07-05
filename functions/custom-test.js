e710531cf3244df5d4f425c5bc71c54f44ff0846
Po Rith <poo@Poamrongs-MBP.attlocal.net>
2019-07-04 22:45:32 -0700

    update functions

--- functions/custom-test.js
+++ functions/custom-test.js
@@ -1,7 +1,3 @@
-import faunadb from 'faunadb' /* Import faunaDB sdk */
-
-process.env.FAUNADB_SERVER_SECRET = 'fnADShQvS9ACAHyTZ1e6xHU9TCwXuwyN6N2o2HaU'
-process.env.FAUNADB_SECRET = 'fnADShQvS9ACAHyTZ1e6xHU9TCwXuwyN6N2o2HaU'
 
 
 exports.handler = (event, context, callback) => {
