5796aeb548fe45428a0450d8a0f1530a5b02635e
Po Rith <poo@Poamrongs-MBP.attlocal.net>
2019-07-04 22:50:04 -0700

    update functions

--- functions/custom-test.js
+++ functions/custom-test.js
@@ -1,9 +1,39 @@
+import faunadb from 'faunadb' /* Import faunaDB sdk */
+
+process.env.FAUNADB_SERVER_SECRET = 'fnADShQvS9ACAHyTZ1e6xHU9TCwXuwyN6N2o2HaU'
+process.env.FAUNADB_SECRET = 'fnADShQvS9ACAHyTZ1e6xHU9TCwXuwyN6N2o2HaU'
 
 
+
+/* configure faunaDB Client with our secret */
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET
+})
+
+/* export our lambda function as named "handler" export */
 exports.handler = (event, context, callback) => {
-    callback(null, {
+  /* parse the string body into a useable JS object */
+  const data = JSON.parse(event.body)
+  console.log('Function `todo-create` invoked', data)
+  const todoItem = {
+    data: data
+  }
+  /* construct the fauna query */
+  return client.query(q.Create(q.Ref('classes/todos'), todoItem))
+    .then((response) => {
+      console.log('success', response)
+      /* Success! return the response with statusCode 200 */
+      return callback(null, {
         statusCode: 200,
-        body: 'Everything is okay!!'
+        body: JSON.stringify(response)
+      })
+    }).catch((error) => {
+      console.log('error', error)
+      /* Error! return the error with statusCode 400 */
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      })
     })
 }
-
