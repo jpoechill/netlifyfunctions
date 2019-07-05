ed6e824b24323063903b59b0f2fca19cbcb39c0e
Po Rith <poo@Poamrongs-MBP.attlocal.net>
2019-07-04 22:55:32 -0700

    update functions

--- functions/custom-test.js
+++ functions/custom-test.js
@@ -4,18 +4,18 @@ const faunadb = require("faunadb");
 process.env.FAUNADB_SERVER_SECRET = 'fnADShQvS9ACAHyTZ1e6xHU9TCwXuwyN6N2o2HaU'
 process.env.FAUNADB_SECRET = 'fnADShQvS9ACAHyTZ1e6xHU9TCwXuwyN6N2o2HaU'
 
-exports.handler = (event, context, callback) => {
-    callback(null, {
-        statusCode: 200,
-        body: 'Everything is okay!!'
-    })
-}
+// exports.handler = (event, context, callback) => {
+//     callback(null, {
+//         statusCode: 200,
+//         body: 'Everything is okay!!'
+//     })
+// }
 
-// /* configure faunaDB Client with our secret */
-// const q = faunadb.query
-// const client = new faunadb.Client({
-//   secret: process.env.FAUNADB_SERVER_SECRET
-// })
+/* configure faunaDB Client with our secret */
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET
+})
 
 /* export our lambda function as named "handler" export */
 exports.handler = (event, context, callback) => {
